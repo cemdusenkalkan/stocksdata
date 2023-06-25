cfd35d3f7d5a89b187800023a36fc10c3694db23
cemd <99793829+cemdusenkalkan@users.noreply.github.com>
2023-06-25 18:17:26 +0300

    Added calculate_returns function

--- main.py
+++ main.py
@@ -8,4 +8,18 @@ print(data)
 
 data.Close.plot()
 plt.show()
+    def calculate_returns(ticker, investment, purchase_date, sell_date):
+        data = yf.download(ticker, purchase_date, sell_date)
+        purchase_price = data.iloc[0]['Close']
+        sell_price = data.iloc[-1]['Close']
+
+        shares_bought = investment / purchase_price
+        investment_value = shares_bought * sell_price
+
+        return investment_value
+
+    #This function downloads the data and tracks the returns. Number of 
+shares can be found by a basic division.git status -s main.py
+
+
 
